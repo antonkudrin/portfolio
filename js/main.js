1bb2076582ab68b701f0d623ac1176c4e1a2297d
antonkudrin <atlet-2@mail.ru>
2020-06-10 15:21:11 +0500

    new vodeofon

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
+
+  //! Подключения видео фона
+  $("#header").vide("./video/Codes", {
+    bgColor: "rgba(0, 0, 0, 0.7)",
+  });
+  
   $(window).on("load", function () {
     //! Подлюченикя прокрутки по #ID
     $("a[rel='mPageScroll2id'], a[rel='header'], a[rel='portfolio'").mPageScroll2id();
+  });
 
     //! WOW Анимация
     new WOW().init();
 
 
-    //! Подключения видео фона
-    $("#header").vide("./video/natyre", {
-      bgColor: "rgba(0, 0, 0, 0.7)",
-    });
-  });
+
 });
