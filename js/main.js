a4f133d18861749f283798cb82c9e02717fe755c
antonkudrin <atlet-2@mail.ru>
2020-06-20 01:20:27 +0500

    About

--- js/main.js
+++ js/main.js
@@ -5,10 +5,26 @@ $(document).ready(function () {
   });
 
   //! Подлюченикя прокрутки по #ID
-  $(
-    "a[rel='mPageScroll2id'], a[rel='header'], a[rel='portfolio'"
-  ).mPageScroll2id();
+  $("a[rel='about'], a[rel='header']").mPageScroll2id();
 
   //! WOW Анимация
   new WOW().init();
+
+  $(window).scroll(function () {
+    var st = $(this).scrollTop();
+
+    $("#avatar").css({
+      transform: "translate(0%, " + st / 3 + "%)",
+    });
+
+    $("#header_title").css({
+      transform: "translate(0%, " + st / 1.6 + "%)",
+    });
+
+    $("#header_subtitle").css({
+      transform: "translate(0%, " + st / 1.2 + "%)",
+    });
+
+   
+  });
 });
